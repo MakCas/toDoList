3643d5d91bd044a7b6da52c59c41ec72a697e28a
Mak_whisk <maklashov.ivan@gmail.com>
2021-12-31 20:36:01 +0300

    Lesson-1: refactored code according to swiftLint

--- toDoList/SceneDelegate.swift
+++ toDoList/SceneDelegate.swift
@@ -8,17 +8,18 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-    
-    var window: UIWindow?
-    
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
 
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
         guard let windowScene = scene as? UIWindowScene else { return }
         window = UIWindow(windowScene: windowScene)
-        let vc = ViewController()
-        window!.rootViewController = vc
+        let viewController = ViewController()
+        window!.rootViewController = viewController
         window!.makeKeyAndVisible()
     }
 }
-
