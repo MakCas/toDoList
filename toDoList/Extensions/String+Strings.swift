cebca3ff7dc2acf59189df1a6b2aa8cd7e2767ff
Mak_whisk <maklashov.ivan@gmail.com>
2022-01-02 09:18:33 +0300

    Lesson-3: created  strings for CreateTaskController

--- toDoList/Extensions/String+Strings.swift
+++ toDoList/Extensions/String+Strings.swift
@@ -9,7 +9,15 @@ import Foundation
 
 extension String {
 
-    enum ViewController {
-        static let appVersion =  Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+    enum CreateTaskController {
+        static let cancelButtonText = "Отменить"
+        static let nameScreenLabelText = "Дело"
+        static let saveButtonText = "Сохранить"
+
+        static let taskTextViewPlaceHolder = "Что надо сделать"
+        static let importanceLabelText = "Важность"
+        static let importanceNoLabelText = "Нет"
+        static let deadLineLabelText = "Сложность до"
+        static let deleteButtonText = "Удалить"
     }
 }
