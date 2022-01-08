e15990a222e4b668dc2bcd7d04b24ce46b8d1ed3
Mak_whisk <maklashov.ivan@gmail.com>
2022-01-08 13:18:17 +0300

    Lesson-4_task1: added separator for tableView with necessary insets including removing it from last cell

--- toDoList/Modules/AllTasksScreen/Views/TaskCell.swift
+++ toDoList/Modules/AllTasksScreen/Views/TaskCell.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 protocol TaskCellDelegate: AnyObject {
-
+    
     func statusChangedFor(taskID: String)
 }
 
@@ -20,22 +20,22 @@ enum TypeCell {
 }
 
 final class TaskCell: UITableViewCell {
-
+    
     // MARK: - Layout and Constants
-
+    
     private enum Layout {
-
+        
     }
-
+    
     // MARK: - Subviews
-
+    
     private lazy var checkControl: CheckControl = {
         let control = CheckControl()
         control.addTarget(self, action: #selector(controlTapped(sender:)), for: .touchUpInside)
         control.translatesAutoresizingMaskIntoConstraints = false
         return control
     }()
-
+    
     private lazy var stackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .vertical
@@ -43,7 +43,7 @@ final class TaskCell: UITableViewCell {
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
-
+    
     private lazy var taskLabel: UILabel = {
         let label = UILabel()
         label.text = "taskLabel"
@@ -51,13 +51,13 @@ final class TaskCell: UITableViewCell {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-
+    
     private lazy var deadLineLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-
+    
     private lazy var chevronImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(systemName: "chevron.right")
@@ -65,43 +65,43 @@ final class TaskCell: UITableViewCell {
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
-
+    
     // MARK: - Properties
-
+    
     private var typeCell: TypeCell?
     private var taskCellViewModel: TaskCellViewModel?
     weak var delegate: TaskCellDelegate?
-
+    
     // MARK: - Init
-
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super .init(style: style, reuseIdentifier: reuseIdentifier)
-
+        
         configureUI()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     override func layoutSubviews() {
         super.layoutSubviews()
-
-        setMaskedCorners(for: typeCell ?? .last)
+        
+        
     }
-
+    
     // MARK: - UI
-
+    
     private func configureUI() {
         selectionStyle = .none
         backgroundColor = .clear
         contentView.backgroundColor = .white
         contentView.layer.cornerRadius = 15
-
+        
         addSubviews()
         addConstraints()
     }
-
+    
     private func addSubviews() {
         contentView.addSubview(checkControl)
         contentView.addSubview(stackView)
@@ -109,30 +109,30 @@ final class TaskCell: UITableViewCell {
         stackView.addArrangedSubview(taskLabel)
         stackView.addArrangedSubview(deadLineLabel)
     }
-
+    
     private func addConstraints() {
-
+        
         let heightConstraint = checkControl.heightAnchor.constraint(equalToConstant: 30)
         heightConstraint.priority = .defaultHigh
-
+        
         NSLayoutConstraint.activate([
             checkControl.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15),
             heightConstraint,
             checkControl.widthAnchor.constraint(equalToConstant: 30),
             checkControl.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-
+            
             chevronImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
             chevronImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15),
-
+            
             stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15),
             stackView.leadingAnchor.constraint(equalTo: checkControl.trailingAnchor, constant: 10),
             stackView.trailingAnchor.constraint(equalTo: chevronImageView.trailingAnchor, constant: -10),
             stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -15)
         ])
     }
-
+    
     // MARK: - Configure
-
+    
     func configureCellWith(model: TaskCellViewModel, typeCell: TypeCell) {
         taskCellViewModel = model
         taskLabel.attributedText = model.itemText
@@ -144,12 +144,14 @@ final class TaskCell: UITableViewCell {
         }
         checkControl.isSelected = model.isDone
         self.typeCell = typeCell
-        setMaskedCorners(for: typeCell)
+        setMaskedCornersAndSeparator(for: typeCell)
+        
     }
-
+    
     // MARK: - Private Functions
-
-    private func setMaskedCorners(for typeCell: TypeCell) {
+    
+    private func setMaskedCornersAndSeparator(for typeCell: TypeCell) {
+        separatorInset = UIEdgeInsets(top: 0, left: 10 + 30 + 15, bottom: 0, right: 0)
         switch typeCell {
         case .first:
             contentView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
@@ -157,11 +159,12 @@ final class TaskCell: UITableViewCell {
             contentView.layer.maskedCorners = []
         case .last:
             contentView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+            self.separatorInset.right = CGFloat.greatestFiniteMagnitude
         case .willAllCorners:
             contentView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner, .layerMaxXMinYCorner, .layerMinXMinYCorner]
         }
     }
-
+    
     @objc private func controlTapped(sender: UIControl) {
         guard let model = taskCellViewModel else { return }
         delegate?.statusChangedFor(taskID: model.id)
